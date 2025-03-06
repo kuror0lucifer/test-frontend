58b0f1fbd222233f3f45a91a17da5450d7723803
kuror0lucifer <kovalishin.2014@mail.ru>
2025-03-06 16:50:17 +0200

    улучшена функция форматирования даты, теперь она работает с любым форматом даты, полученной с апи

--- kode-intership-2025-react/src/utils/formatDate.ts
+++ kode-intership-2025-react/src/utils/formatDate.ts
@@ -1,12 +1,20 @@
 import { format } from 'date-fns';
 import { ru } from 'date-fns/locale';
 
-const formatDate = (date: string): string => {
-  const [year, month, day] = date.split('-').map(x => Number(x));
-  const birthday = new Date(year, month, day);
-  const formatedDate = format(birthday, 'dd MMMM yyyy', { locale: ru });
+const formatDate = (date: string | Date): string => {
+  let parsedDate: Date | null = null;
 
-  return formatedDate;
+  if (date instanceof Date) {
+    parsedDate = date;
+  } else if (typeof date === 'string') {
+    parsedDate = new Date(date);
+  }
+
+  if (!parsedDate) {
+    throw new Error('Неверный формат даты');
+  }
+
+  return format(parsedDate, 'dd MMMM yyyy', { locale: ru });
 };
 
 export default formatDate;
